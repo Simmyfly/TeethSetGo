a27c8fddb57e0a342bc90467de5a37cdcc5b945d
Simin Huang <shua0012@student.monash.edu>
2018-08-28 14:47:40 +1000

    First commit

---
+++ IEApplication/Content/js/showQuestion.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+$('.notUrgent').click(function () {
+    document.getElementById('courses').style.display = 'none';
+    document.getElementById('feature').style.display = 'inline';
+    });
+    $(document).on('click', '#goBackButton', function () {
+        document.getElementById('courses').style.display = 'inline';
+        document.getElementById('feature').style.display = 'none';
+    });
+});
---
+++ IEApplication/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IEApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EmergencyPage()
+        {
+            ViewBag.Message = "Map showing nearest dental clinics.";
+
+            return View();
+        }
+
+        public ActionResult GeneralPage()
+        {
+            ViewBag.Message = "Map showing dental clinics with waiting time.";
+
+            return View();
+        }
+    }
+}
